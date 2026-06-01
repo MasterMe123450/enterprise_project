f61acfb0b5634e376377e4f9ab647766db1457e8
ethan.tay1@education.nsw.gov.au <ethan.tay1@education.nsw.gov.au>
2026-06-01 06:28:14 +0000

    Edited settings, LogIn_Page and Dashboard_Page

--- client_code/Dashboard_Page/__init__.py
+++ client_code/Dashboard_Page/__init__.py
@@ -1,5 +1,9 @@
 from ._anvil_designer import Dashboard_PageTemplate
 from anvil import *
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+import anvil.users
 
 class Dashboard_Page(Dashboard_PageTemplate):
   def __init__(self, **properties):
--- client_code/LogIn_Page/__init__.py
+++ client_code/LogIn_Page/__init__.py
@@ -1,5 +1,9 @@
 from ._anvil_designer import LogIn_PageTemplate
 from anvil import *
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+import anvil.users
 
 
 class LogIn_Page(LogIn_PageTemplate):
