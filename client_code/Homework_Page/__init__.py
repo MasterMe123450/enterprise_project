5f90268f1d092a32ec27ae67a4cf3052ff38aebe
ethan.tay1@education.nsw.gov.au <ethan.tay1@education.nsw.gov.au>
2026-06-02 04:59:09 +0000

    Edited settings and Homework_Page

--- client_code/Homework_Page/__init__.py
+++ client_code/Homework_Page/__init__.py
@@ -12,10 +12,13 @@ class Homework_Page(Homework_PageTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     super().__init__(**properties)
+    
     if anvil.server.call('tutor_perms'):
       self.tutor_redirect.visible = True
     # Any code you write here will run before the form opens.
 
+    
+    
   @handle("dashboard_redirect", "click")
   def dashboard_redirect_click(self, **event_args):
     """This method is called when the button is clicked"""
@@ -37,9 +40,37 @@ class Homework_Page(Homework_PageTemplate):
     """This method is called when a new file is loaded into this FileLoader"""
     upload_row = app_tables.finishedhomeworkfiles.add_row()
     upload_row['Homework_File'] = self.Homework_Upload.file
-   
+    upload_row['Homework_Title'] = self.Homework_Upload.file.name
     upload_row['Uploader'] = anvil.users.get_user()
     
-    date = datetime.datetime.now()
+    date = datetime.now()
     upload_row['Upload Date'] = date
+  
+  @handle("file_name_input", "pressed_enter")
+  def file_name_input_pressed_enter(self, **event_args):
+    """This method is called when the user presses Enter in this text box"""
+    upload_row = app_tables.finishedhomeworkfiles.get(Homework_Title = self.Homework_Upload.file.name)
+    upload_row['Homework_Title'] = self.file_name_input.text
+
+  @handle("homework_dropdown", "show")
+  def homework_dropdown_show(self, **event_args):
+    """This method is called when the DropDown is shown on the screen"""
+    worksheetlist = [""]
+    for row in app_tables.homeworkfiles.search():
+     worksheetlist.append(row["Homework_Title"])
+    self.homework_dropdown.items = worksheetlist
+
+  @handle("homework_dropdown", "change")
+  def homework_dropdown_change(self, **event_args):
+    """This method is called when an item is selected"""
+    upload_row = app_tables.finishedhomeworkfiles.get(Homework_Title = self.Homework_Upload.file.name)
+    upload_row['Homework_Title'] = self.homework_dropdown.selected_value
+
+  @handle("doohickey", "click")
+  def doohickey_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    self.feedback_label.visible = True
+    
+    
+    
     
