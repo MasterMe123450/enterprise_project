f77d2140ed352e48dac00b4fae48932b3b011dec
ethan.tay1@education.nsw.gov.au <ethan.tay1@education.nsw.gov.au>
2026-06-11 09:32:18 +0000

    Edited LogIn_Page and Specific_Statistics_Page

--- client_code/LogIn_Page/__init__.py
+++ client_code/LogIn_Page/__init__.py
@@ -12,6 +12,11 @@ class LogIn_Page(LogIn_PageTemplate):
     # Set Form properties and Data Bindings.
     super().__init__(**properties)
     #prompt login
+    # Any code you write here will run before the form opens.
+
+  @handle("label_1", "show")
+  def label_1_show(self, **event_args):
+    """This method is called when the Label is shown on the screen"""
     anvil.users.login_with_form()
     #redirect and create new if new
     if anvil.users.get_user() is not None:
@@ -20,7 +25,6 @@ class LogIn_Page(LogIn_PageTemplate):
       if currentuserhwdata is None:
         app_tables.homework.add_row(Student=currentuser)
       open_form('Dashboard_Page')
-      
+
     else: 
       anvil.users.login_with_form()
-    # Any code you write here will run before the form opens.
--- client_code/Specific_Statistics_Page/__init__.py
+++ client_code/Specific_Statistics_Page/__init__.py
@@ -1,5 +1,6 @@
 from ._anvil_designer import Specific_Statistics_PageTemplate
 from anvil import *
+import plotly.graph_objects as go
 import anvil.server
 import anvil.users
 import anvil.tables as tables
@@ -22,16 +23,49 @@ class Specific_Statistics_Page(Specific_Statistics_PageTemplate):
   def Load_Button_click(self, **event_args):
     """This method is called when the button is clicked"""
     tablecontents = []
+    marklist = []
+    self.data_grid_1.visible = True
+    self.Distribution_Plot.visible = True
+    self.flow_panel_1.visible = True
+
+    #The marklist(tm)
     for row in app_tables.finishedhomeworkfiles.search():
       if row['Homework_Title'] == self.Selection_Dropdown.selected_value:
         contentdict = {}
         user = row['Uploader']
         contentdict["Uploader"] = user['Name']
         contentdict["Marks"] = row['Marks']
+        marklist.append(row['Marks'])
         tablecontents.append(contentdict)
     tablecontents.sort(key=lambda contentdict: contentdict["Marks"], reverse=True)
     self.repeating_panel_1.items = tablecontents
 
+    #The KPIs
+    marklist.sort()
+    averagemark = 0
+    for i in marklist:
+      averagemark += i
+    averagemark/=len(marklist)
+    if len(marklist) % 2 == 0:
+      index = len(marklist)/2
+      medianmark = marklist[int(index)]
+    elif len(marklist) != 1:
+      index = len(marklist)/2 + 0.5
+      medianmark = marklist[int(index)]
+      index = len(marklist)/2 - 0.5
+      medianmark += marklist[int(index)]
+      medianmark /= 2
+    else:
+      medianmark = marklist[0]
+    self.n_KPI.text = "Number of Students: " + str(len(marklist))
+    self.Average_Mark_KPI.text = "Average Mark: " + str(round(averagemark,2))
+    self.Median_KPI.text = "Median Mark: " + str(medianmark)
+    self.Mode_KPI.text= "Mode: " + str(max(set(marklist), key = marklist.count))
+
+    #The distribution plot
+    self.Distribution_Plot.data = [
+      go.Box(x = marklist)
+    ]
   @handle("Selection_Dropdown", "show")
   def Selection_Dropdown_show(self, **event_args):
     """This method is called when the DropDown is shown on the screen"""
@@ -70,4 +104,10 @@ class Specific_Statistics_Page(Specific_Statistics_PageTemplate):
   def Statistics_Redirect2_click(self, **event_args):
     """This method is called when the button is clicked"""
     open_form('Statistics_Page')
+
+  @handle("Logout_Button", "click")
+  def Logout_Button_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    anvil.users.logout()
+    open_form('LogIn_Page')
 
